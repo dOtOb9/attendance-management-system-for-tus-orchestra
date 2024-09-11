495853a2ff27c06d3606cb1ec309bc9c58673327
dOtOb9 <masa.taga0528@gmail.com>
2024-09-11 09:45:01 +0900

    classの階層構造の修正

--- src/main.ts
+++ src/main.ts
@@ -102,74 +102,73 @@ class TuttiAttendanceBook extends AttendanceBook {
     }
 }
 
-  class Sheet {
-    private sheet: GoogleAppsScript.Spreadsheet.Sheet;
-    protected data: string[][];
+class Sheet {
+  private sheet: GoogleAppsScript.Spreadsheet.Sheet;
+  protected data: string[][];
+
+  constructor(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
+    this.sheet = sheet;
+
+    // 入力されている右端のセルを取得
+    const last_row = sheet.getLastRow();
+    const last_col = sheet.getLastColumn();
+
+    // 入力されているデータ全体を取得
+    this.data = sheet.getRange(1, 1, last_row, last_col).getDisplayValues();
+
+    // ==================================================================================
+    const addRowNumbers = (matrix: string[][]): string[][] => {
+      return matrix.map((row, index) => [(index + 1).toString(), ...row]);
+    };
+
+    const addColumnNumbers = (matrix: string[][]): string[][] => {
+      const columnNumbers = Array.from({ length: matrix[0].length + 1 }, (_, index) => index.toString());
+      return [columnNumbers, ...matrix];
+    };
+    // ==================================================================================
+
+    this.data = addRowNumbers(this.data);
+    this.data = addColumnNumbers(this.data);
+  }
   
-    constructor(sheet: GoogleAppsScript.Spreadsheet.Sheet) {
-      this.sheet = sheet;
-  
-      // 入力されている右端のセルを取得
-      const last_row = sheet.getLastRow();
-      const last_col = sheet.getLastColumn();
-  
-      // 入力されているデータ全体を取得
-      this.data = sheet.getRange(1, 1, last_row, last_col).getDisplayValues();
-  
-      // ==================================================================================
-      const addRowNumbers = (matrix: string[][]): string[][] => {
-        return matrix.map((row, index) => [(index + 1).toString(), ...row]);
-      };
-  
-      const addColumnNumbers = (matrix: string[][]): string[][] => {
-        const columnNumbers = Array.from({ length: matrix[0].length + 1 }, (_, index) => index.toString());
-        return [columnNumbers, ...matrix];
-      };
-      // ==================================================================================
-  
-      this.data = addRowNumbers(this.data);
-      this.data = addColumnNumbers(this.data);
-    }
-    
-    protected createColumnsLeft(value: string, col: number, rows: number): number {
-      this.sheet.insertColumnsBefore(col, rows);
-      this.sheet.getRange(1, col).setValue(value);
-      return this.data[0].length - col;
-    }
-  
-    protected setValue(row: number, col: number, value: string): void {
-      this.sheet.getRange(row, col).setValue(value);
-    }
-  
-    protected setValues(startRow: number, startCol: number, NumRows: number, NumCols: number, values: Array<Array<string>>): void {
-      this.sheet.getRange(startRow, startCol, NumRows, NumCols).setValues(values);
-    }
-  
-    protected appendRow(row: Array<string>): void {
-      this.sheet.appendRow(row);
-    }
+  protected createColumnsLeft(value: string, col: number, rows: number): number {
+    this.sheet.insertColumnsBefore(col, rows);
+    this.sheet.getRange(1, col).setValue(value);
+    return this.data[0].length - col;
   }
 
-  
-  class MemberSheet extends Sheet {
-    public editMember(memberRow: Array<string>): void {
-        const userRow = this.searchMember(memberRow[1]);
+  protected setValue(row: number, col: number, value: string): void {
+    this.sheet.getRange(row, col).setValue(value);
+  }
 
-        if (userRow === undefined) {
-            this.addMember(memberRow);
-            return;
-        }
-    }
-    
-    private addMember(memberRow: Array<string>) {
-        this.appendRow(memberRow);
-    }
-    
-    protected searchMember(id: string) {
-        const memberRow = this.data.find((row) => row[2] === id);
-        
-        return memberRow;
-    }
+  protected setValues(startRow: number, startCol: number, NumRows: number, NumCols: number, values: Array<Array<string>>): void {
+    this.sheet.getRange(startRow, startCol, NumRows, NumCols).setValues(values);
+  }
+
+  protected appendRow(row: Array<string>): void {
+    this.sheet.appendRow(row);
+  }
+}
+  
+class MemberSheet extends Sheet {
+  public editMember(memberRow: Array<string>): void {
+      const userRow = this.searchMember(memberRow[1]);
+
+      if (userRow === undefined) {
+          this.addMember(memberRow);
+          return;
+      }
+  }
+  
+  private addMember(memberRow: Array<string>) {
+      this.appendRow(memberRow);
+  }
+  
+  protected searchMember(id: string) {
+      const memberRow = this.data.find((row) => row[2] === id);
+      
+      return memberRow;
+  }
 }
 
 class AttendanceCodeSheet extends Sheet {
